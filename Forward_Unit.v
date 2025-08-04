c12c59454cda0aee6ffae12cf142b8897ee06aa9
RohanRudra <rohanrudra0914@gmail.com>
2025-08-04 21:07:11 +0530

    Forwarding unit update

--- Forward_Unit.v
+++ Forward_Unit.v
@@ -5,14 +5,12 @@ module Forward_Unit(ID_EX_RegRs1, ID_EX_RegRs2, EX_MEM_RegW, EX_MEM_RegRd, MEM_W
 
     //For Mux_A
     assign Mux_A = (EX_MEM_RegW && (EX_MEM_RegRd != 5'd0) && (EX_MEM_RegRd == ID_EX_RegRs1)) ? 2'b10 :
-                   (MEM_WB_RegW && (MEM_WB_RegRd != 5'd0) && (MEM_WB_RegRd == ID_EX_RegRs1)) ? 2'b01 : 
-                   2'b00;
+                   (MEM_WB_RegW && (MEM_WB_RegRd != 5'd0) && (MEM_WB_RegRd == ID_EX_RegRs1)
+                   && !(EX_MEM_RegW && (EX_MEM_RegRd != 5'd0) && (EX_MEM_RegRd == ID_EX_RegRs1))) ? 2'b01 : 2'b00;
 
     //For Mux_B
     assign Mux_B = (EX_MEM_RegW && (EX_MEM_RegRd != 5'd0) && (EX_MEM_RegRd == ID_EX_RegRs2)) ? 2'b10 : 
-                   (MEM_WB_RegW && (MEM_WB_RegRd != 5'd0) && (MEM_WB_RegRd == ID_EX_RegRs2)) ? 2'b01 : 
-                   2'b00;
-
-    //There are few conditions left
+                   (MEM_WB_RegW && (MEM_WB_RegRd != 5'd0) && (MEM_WB_RegRd == ID_EX_RegRs2)
+                   && !(EX_MEM_RegW && (EX_MEM_RegRd != 5'd0) && (EX_MEM_RegRd == ID_EX_RegRs2))) ? 2'b01 : 2'b00;
 
 endmodule
